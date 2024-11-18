c1b530fbb95b7b8bf6384dbabae7af40b3cc40e6
ivandersirait <ivandersirait@gmail.com>
2024-11-18 11:15:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle navigation menu for mobile devices
+const hamburger = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
